36378f8ab182dd0352e940b8fa96c8db92569ecb
Nelson <nelsonmokoro37@gmail.com>
2022-03-30 12:59:20 +0300

    success

--- base/forms.py
+++ base/forms.py
@@ -13,5 +13,5 @@ class RoomForm(ModelForm):
 class UserForm(ModelForm):
     class Meta:
         model = User
-        fields = ['username', 'email']
+        fields = ['username', 'email','first_name', 'last_name']
         
